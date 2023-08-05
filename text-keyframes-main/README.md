f59f1288b3aceab8b936a55826a734d4a37d3360
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2023-08-06 01:05:55 +0500

    text kayframes javascript

---
+++ text-keyframes-main/README.md
@@ -0,0 +1,17 @@
+# text-keyframes
+new project full version
+
+
+<h2>to used langue 👇👇</h2>
+<br/>
+
+
+
+[![SkillIcons](https://skillicons.dev/icons?i=html,css,js)](https://skillicons.dev)<br/>
+
+
+
+<h1>to view online 👇👇</h1>
+<br/>
+
+https://stellar-kataifi-c4edd2.netlify.app/
---
+++ text-keyframes-main/scrit.js
@@ -0,0 +1,20 @@
+const body = document.querySelector('body');
+body.style.margin  = "0";
+body.style.padding = "0";
+body.style.display = "flex";
+body.style.justifyContent = "center";
+body.style.alignItems = "center";
+body.style.height = "100vh";
+body.style.background = "#000";
+
+
+
+const p = document.querySelector('p');
+p.style.position = "relative";
+p.style.fontFamily = "sans-serif";
+p.style.textTransform = "uppercase";
+p.style.fontSize = "30px";
+p.style.letterSpacing = "4px";
+p.style.overflow = "hidden";
+p.style.backgroundRepeat = "no-repeat";
+p.style.backgroundSize = "80%";
