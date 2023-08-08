34a33155ea270ec7b21ef812196dba99707a0722
Shakhzodbee <Bakhodirovichshakhzodbek@gmail.com>
2023-08-09 00:07:51 +0500

    Quiz application (Ozbekcha)

---
+++ Quiz-app-main/README.md
@@ -0,0 +1,16 @@
+# Quiz-app
+new project
+
+
+<h2>to used langue 👇👇</h2>
+<br/>
+
+
+[![SkillIcons](https://skillicons.dev/icons?i=html,css,js)](https://skillicons.dev)<br/>
+
+
+
+<h1>to view online 👇👇</h1>
+<br/>
+
+https://jazzy-paprenjak-0675e2.netlify.app/
---
+++ Quiz-app-main/question1.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Ozbekiston birinchi prezisidenti kim bolgan",
+    answer: "Islom Abduganiyev Karimov",
+    options: [
+      "Abdulla Oripov",
+      "Abdulla Avloniy",
+      "Islom Abduganiyev Karimov",
+      "Shakvat Miromonovich Mirziyoyev"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Ozbekistonda nechta viloyat bor ?",
+    answer: "12",
+    options: [
+      "11",
+      "12",
+      "13",
+      "9"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Ozbekiston qayerda joylashgan",
+    answer: "Osiyo",
+    options: [
+      "Osiyo",
+      "Afrika",
+      "Antarktida",
+      "Yevropa"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Ozbekiston bayrogida nechta rang tasvirlangan",
+    answer: "4",
+    options: [
+      "3",
+      "5",
+      "1",
+      "4",
+    ]
+  },
+  {
+    numb: 5,
+    question: "Ozbek millati si'ginadigan din",
+    answer: "Islom",
+    options: [
+      "Yahudiy",
+      "Islom",
+      "Nasroniy",
+      "xech qanday dinga"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Ozbekistonda talim tizimi necha yil davom etadi",
+    answer: "11-yil",
+    options: [
+      "15-yil",
+      "11-yil",
+      "17-yil",
+      "9-yil"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Ozbekistonda 2007-yil qanday nom berilgan (Yil nomi)",
+    answer: "Ijtimoiy himoya yili",
+    options: [
+      "Yoshlar yili",
+      "Ijtimoiy himoya yili",
+      "Obod turmush yili",
+      "Amir Temur yili"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Ozbek boksyorini tanlang",
+    answer: "Hasanboy Dostmatov",
+    options: [
+      "Mahmud Murodov",
+      "Hasanboy Dostmatov",
+      "Islam Makhachev",
+      "Anzor Azhiyev"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Ozbekistonda moshina ishlab chiqariladigan zavod nomi",
+    answer: "GM",
+    options: [
+      "GM",
+      "Daewoo",
+      "Ford",
+      "Lada"
+    ]
+  },
+  {
+    numb:10,
+    question: "2030-yilda Ozbekiston mustaqilligining necha yilligi nishonlanadi",
+    answer: "39-yilligi",
+    options: [
+      "46-yilligi",
+      "39-yilligi",
+      "15-yilligi",
+      "38-yilligi"
+    ]
+  }
+];
